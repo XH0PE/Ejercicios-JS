74dc7a2f2e52e412d418321962b3b02d245849ac
diego <pecioso999@gmai.com>
2023-03-17 14:33:17 -0500

    ejercicios terminados

---
+++ boton.js
@@ -0,0 +1,6 @@
+// Crea un botón en HTML y utiliza JavaScript para detectar cuando se hace clic en él y mostrar un mensaje en la consola.
+    const miBtn = document.getElementById('miBoton');
+
+    miBtn.addEventListener('click', () => {
+    console.log('mensaje')
+})
